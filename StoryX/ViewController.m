23c8353772c377cf25c944be73e812f41f1fef78
wqxhouse <wqxhouse1@gmail.com>
2014-10-25 21:40:25 -0700

    Initial Commit

---
+++ StoryX/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StoryX
+//
+//  Created by Robin Wu on 10/25/14.
+//  Copyright (c) 2014 WSH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
